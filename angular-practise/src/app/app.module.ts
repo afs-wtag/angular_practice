0648acdd27739d55004e53c2ae27db2f3ac337a5
afs-wtag <shafia.afrin@welldev.io>
2021-08-29 18:26:31 +0600

    implemented routes

--- angular-practise/src/app/app.module.ts
+++ angular-practise/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+// import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    // ProductListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
